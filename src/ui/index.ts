b7a5428bdb420b76ef7fbb4ff9c93c6df1f7b87e
Sanensi <william.picard@hotmail.com>
2022-10-15 11:01:26 -0400

    feature: Update ui to handle ambiguous search result

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 
 import recipes from "../../data/recipes.json";
-import { itemName } from "../domain/Recipe";
+import { ItemName, itemName } from "../domain/Recipe";
 import { ReportUseCase } from "../use-cases/ReportUseCase";
 const [q = ""] = process.argv.slice(2);
 
 const search_result = recipes.filter((recipe) => recipe.name.includes(q));
+const exact_search = search_result.filter((recipe) => recipe.name === q);
 
 if (search_result.length === 1) {
-  const [recipe] = search_result;
-  const report = new ReportUseCase().getReportFor(itemName(recipe.name));
+  displayInputOutputReport(itemName(search_result[0].name));
+} else if (exact_search.length === 1) {
+  displayInputOutputReport(itemName(exact_search[0].name));
+} else {
+  console.log(search_result.map((result) => result.name).sort());
+}
+
+function displayInputOutputReport(recipeName: ItemName) {
+  const report = new ReportUseCase().getReportFor(recipeName);
+
   const display = {
     ...report,
     inputs: report.inputs.map(([name, unitPerSecond]) => [
@@ -21,7 +30,6 @@ if (search_result.length === 1) {
       unitPerSecond.numerator / unitPerSecond.denominator,
     ]),
   };
+
   console.log(display);
-} else {
-  console.log(search_result.map((result) => result.name).sort());
 }
